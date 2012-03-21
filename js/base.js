b5f2660368503fe5c49bfdf759ae45a0b6807b28
liu yang <zation1@gmail.com>
2012-03-22 00:37:55 +0800

    click point to see detials in dialog

--- js/base.js
+++ js/base.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
+  var is_dragging = false;
+
   function initialize_points() {
-    for (var point_name in db) {
-      if (db.hasOwnProperty(point_name)) {
-        var point = db[point_name];
+    for (var point_name in _db) {
+      if (_db.hasOwnProperty(point_name)) {
+        var point = _db[point_name];
         var point_element = $('<li class="' + point.type + '">' + point_name + '</li>');
+
+        point_element.data('scope', point.scope);
+        point_element.data('quadrant', point.quadrant);
+        point_element.data('description', point.description);
+
         point_element.css({
           'margin-left': -point.coordinate_x,
           'margin-top': -point.coordinate_y
@@ -13,15 +20,16 @@ $(document).ready(function() {
     }
   }
 
-  function bind_events() {
+  function drag_point() {
     var center_x;
     var center_y;
     var current_dragged_point;
     var offset_x;
     var offset_y;
-    var z_index;
+    var z_index = 0;
 
     $('#points').on('mousedown', 'li', function(event) {
+      event.preventDefault();
       current_dragged_point = $(event.currentTarget);
       center_x = $(window).width() / 2;
       center_y = $(window).height() / 2;
@@ -34,6 +42,7 @@ $(document).ready(function() {
       if (current_dragged_point) {
         var left = events.clientX - center_x - offset_x;
         var top = events.clientY - center_y - offset_y;
+        is_dragging = true;
         current_dragged_point.css({
           'margin-left': left,
           'margin-top': top,
@@ -47,8 +56,21 @@ $(document).ready(function() {
     });
   }
 
-  initialize_points();
-  bind_events();
+  function view_point() {
+    $('#points').on('click', 'li', function(event) {
+      if (!is_dragging) {
+        var point_element = $(event.currentTarget);
+        $('#point-detail p').text(point_element.data('description'));
+        $('#point-detail').dialog({
+          title: point_element.text()
+        });
+      }
+      is_dragging = false;
+    });
+  }
 
+  initialize_points();
+  drag_point();
+  view_point();
 
 });
--- js/db.js
+++ js/db.js
@@ -1,16 +1,18 @@
-var db = {
+var _db = {
   'Git': {
     'coordinate_x': 100,
     'coordinate_y': 100,
     'type': 'changed',
     'scope': 'Adopt',
-    'quadrant': 'Tools'
+    'quadrant': 'Tools',
+    'description': 'something about git.'
   },
   'Test recorders': {
     'coordinate_x': 200,
     'coordinate_y': 200,
     'type': 'unchanged',
     'scope': 'Hold',
-    'quadrant': 'Techniques'
+    'quadrant': 'Techniques',
+    'description': 'something about test recorders.'
   }
 };
