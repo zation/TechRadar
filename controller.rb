2f2a456067053a93d1faa875eda3a8c756ff80e8
Liu Yang <zation1@gmail.com>
2012-08-23 21:01:05 +0800

    config redis in heroku

--- controller.rb
+++ controller.rb
@@ -3,7 +3,12 @@ require 'redis'
 
 enable :sessions
 
-redis = Redis.new
+if ENV["REDISTOGO_URL"]
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  redis = Redis.new()
+end
 
 get '/' do
   File.read('login.html')
